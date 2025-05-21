66567e5ac0cde52f947c6e530ea5a6ec93f3e941
hibaettibary <ettibariyhiba@gmail.com>
2025-05-22 00:29:05 +0200

    Modifications locales sur main.dart

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
-import 'screens/welcome_screen.dart';
-import 'screens/login_screen.dart';
-import 'screens/signup_screen.dart';
-
-// Importer aussi home_screen si tu l'as
+import 'package:restaurant_app/screens/Ath/welcome_screen.dart';
+import 'package:restaurant_app/screens/Ath/login_screen.dart';
+import 'package:restaurant_app/screens/Ath/signup_screen.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    initialRoute: '/', // page d’accueil en première
-    routes: {
-      '/': (context) => WelcomeScreen(),
-      '/login': (context) => LoginScreen(),
-      '/signup': (context) => SignUpScreen(),
-    },
-  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/welcome',
+      routes: {
+        '/welcome': (context) => WelcomeScreen(),
+        '/login': (context) => LoginScreen(),
+        '/signup': (context) => SignUpScreen(),
+      },
+    );
+  }
 }
